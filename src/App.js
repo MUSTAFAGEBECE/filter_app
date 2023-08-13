8570b8a67f5543839dd0b998e80be55ad203c634
MUSTAFAGEBECE <mustafagbc003@gmail.com>
2023-08-13 21:31:24 +0300

    gif eklenildi

--- src/App.js
+++ src/App.js
@@ -15,34 +15,3 @@ function App() {
 export default App; 
 
 
-/*
-                        FİLTER_APP  
--{address?.zipcode}
--*****useRef
--******fetch apı
-- const [cloneUsers, setCloneUsers] = useState(null);
-  const [users, setUsers] = useState(null);
-   useEffect(() => {
-     fetch('https://jsonplaceholder.typicode.com/users')
-       .then((res) => res.json())
-       .then((data) => {
-        setUsers(data);
-        setCloneUsers(data);
-      })
-      .catch((err) => alert('Daha sonra tekrar deneyin'));
-  }, []); 
-- const handleSearch = () => {
-    const query = inputRef.current.value.toLowerCase();
-    const filtredUser = cloneUsers.filter((user) =>
-      user.name.toLowerCase().includes(query)
-    );
-    setUsers(filtredUser);
-  };
-- <input type="text" ref={inputRef} onChange={handleSearch} />
-  <button onClick={handleSearch}>Ara</button>
-- <ul>
-        {users === null && <p>Loading...</p>}
-        {users && users.map((user) => <ListElement user={user} />)}
-  </ul>
-                        
-  */
--- src/components/ListElement.js
+++ src/components/ListElement.js
@@ -1,8 +1,7 @@
 import "./filter.css"
 
 const ListElement = ({ user }) => {
-    // yeniden oluşturm (deconstruct)
-    // obje içerisndeki verilere erişme
+    
     const { name, phone, email, address } = user;
   
     return (
