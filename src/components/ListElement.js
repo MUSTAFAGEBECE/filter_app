ce76099b281cd5bc4843bd3194e18b6ea96ba50d
MUSTAFAGEBECE <mustafagbc003@gmail.com>
2023-08-13 20:42:35 +0300

    first commit

---
+++ src/components/ListElement.js
@@ -0,0 +1,19 @@
+import "./filter.css"
+
+const ListElement = ({ user }) => {
+    // yeniden oluşturm (deconstruct)
+    // obje içerisndeki verilere erişme
+    const { name, phone, email, address } = user;
+  
+    return (
+      <li className="userli">
+        Name:{name}
+        <br /><br/>
+       Phone: {phone}
+        <br /><br/>
+       Zipcode: {address?.zipcode}
+      </li>
+    );
+  };
+  
+  export default ListElement;
